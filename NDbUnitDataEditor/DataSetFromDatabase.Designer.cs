532a6dfe78e51927a68188ac8f1437a8e1004c3c
Steve Bohlen <sbohlen@gmail.com>
2009-06-06 23:51:07 +0000

    introduced support for entering connection string and testing the database connection in preparation to suck the dataset out of the database

---
+++ NDbUnitDataEditor/DataSetFromDatabase.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NDbUnitDataEditor
+{
+    partial class DataSetFromDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblConnectionString = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.btnTestConnection = new System.Windows.Forms.Button();
+            this.cboDatabaseType = new System.Windows.Forms.ComboBox();
+            this.lblDatabaseType = new System.Windows.Forms.Label();
+            this.btnFillDataSet = new System.Windows.Forms.Button();
+            this.lblConnectionTestResultMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblConnectionString
+            // 
+            this.lblConnectionString.AutoSize = true;
+            this.lblConnectionString.Location = new System.Drawing.Point(12, 13);
+            this.lblConnectionString.Name = "lblConnectionString";
+            this.lblConnectionString.Size = new System.Drawing.Size(140, 13);
+            this.lblConnectionString.TabIndex = 0;
+            this.lblConnectionString.Text = "Database Connection String";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(158, 10);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(417, 20);
+            this.txtConnectionString.TabIndex = 1;
+            // 
+            // btnTestConnection
+            // 
+            this.btnTestConnection.Location = new System.Drawing.Point(581, 10);
+            this.btnTestConnection.Name = "btnTestConnection";
+            this.btnTestConnection.Size = new System.Drawing.Size(130, 23);
+            this.btnTestConnection.TabIndex = 2;
+            this.btnTestConnection.Text = "Test Connection";
+            this.btnTestConnection.UseVisualStyleBackColor = true;
+            this.btnTestConnection.Click += new System.EventHandler(this.btnTestConnection_Click);
+            // 
+            // cboDatabaseType
+            // 
+            this.cboDatabaseType.FormattingEnabled = true;
+            this.cboDatabaseType.Location = new System.Drawing.Point(158, 37);
+            this.cboDatabaseType.Name = "cboDatabaseType";
+            this.cboDatabaseType.Size = new System.Drawing.Size(196, 21);
+            this.cboDatabaseType.TabIndex = 3;
+            this.cboDatabaseType.SelectedIndexChanged += new System.EventHandler(this.cboDatabaseType_SelectedIndexChanged);
+            // 
+            // lblDatabaseType
+            // 
+            this.lblDatabaseType.AutoSize = true;
+            this.lblDatabaseType.Location = new System.Drawing.Point(12, 40);
+            this.lblDatabaseType.Name = "lblDatabaseType";
+            this.lblDatabaseType.Size = new System.Drawing.Size(80, 13);
+            this.lblDatabaseType.TabIndex = 4;
+            this.lblDatabaseType.Text = "Database Type";
+            // 
+            // btnFillDataSet
+            // 
+            this.btnFillDataSet.Location = new System.Drawing.Point(581, 70);
+            this.btnFillDataSet.Name = "btnFillDataSet";
+            this.btnFillDataSet.Size = new System.Drawing.Size(130, 23);
+            this.btnFillDataSet.TabIndex = 5;
+            this.btnFillDataSet.Text = "Fill DataSet";
+            this.btnFillDataSet.UseVisualStyleBackColor = true;
+            this.btnFillDataSet.Click += new System.EventHandler(this.btnFillDataSet_Click);
+            // 
+            // lblConnectionTestResultMessage
+            // 
+            this.lblConnectionTestResultMessage.AutoSize = true;
+            this.lblConnectionTestResultMessage.Location = new System.Drawing.Point(431, 40);
+            this.lblConnectionTestResultMessage.Name = "lblConnectionTestResultMessage";
+            this.lblConnectionTestResultMessage.Size = new System.Drawing.Size(150, 13);
+            this.lblConnectionTestResultMessage.TabIndex = 6;
+            this.lblConnectionTestResultMessage.Text = "(Not Connected To Database)";
+            // 
+            // DataSetFromDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 105);
+            this.Controls.Add(this.lblConnectionTestResultMessage);
+            this.Controls.Add(this.btnFillDataSet);
+            this.Controls.Add(this.lblDatabaseType);
+            this.Controls.Add(this.cboDatabaseType);
+            this.Controls.Add(this.btnTestConnection);
+            this.Controls.Add(this.txtConnectionString);
+            this.Controls.Add(this.lblConnectionString);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DataSetFromDatabase";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Fill DataSet from Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblConnectionString;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Button btnTestConnection;
+        private System.Windows.Forms.ComboBox cboDatabaseType;
+        private System.Windows.Forms.Label lblDatabaseType;
+        private System.Windows.Forms.Button btnFillDataSet;
+        private System.Windows.Forms.Label lblConnectionTestResultMessage;
+    }
+}
